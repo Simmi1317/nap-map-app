f53171bce9e10ac4fc3f9ae3494b6a41a71e3af2
Simmi1317 <simmi.agarwal1317@gmail.com>
2017-08-14 09:53:24 -0400

    Add files via upload

---
+++ geo-code/menubar/menubar.js
@@ -0,0 +1,7 @@
+document.write('\
+<ul class="topnav">\
+  <li><a class="active" href="https://simmi1317.github.io/nap-map-app/home.html">Home</a></li>\
+  <li><a href="https://simmi1317.github.io/nap-map-app/getnapping.html">Get Napping</a></li>\
+  <li class="right"><a href="https://simmi1317.github.io/nap-map-app/aboutnapmap.html">About NapMap</a></li>\
+</ul>\
+');
